59a489cd9236d9da0317d56ed9a38b7ed4cdf617
Shimizu Yasushi <decode4423@gmail.com>
2020-01-02 04:59:02 +0900

    addDrop_1.0.0

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,9 @@
 #include <WebServer.h>
 #include <WiFi.h>
 
+
+
+
 ///////////Configurable Value///////////
 #define NUM_LED 600
 #define BRIGHTNESS 64
@@ -16,10 +19,20 @@ const char* ssid = "aterm-358916-g";
 const char* pass = "simizu7856";
 int dulationMin = 60;
 int oclock = 17;
+//0=>red, 1=>green, 2=>blue
+int color_temp[3] = {100, 149, 237};
+int color_hum[3] = {0, 191, 255};
+int color_drops[3] = {0, 191, 255};
+// color_temp[0] = 100; color_temp[1] = 149; color_temp[2] = 237;
+// color_hum[0] = 0; color_hum[1] = 191; color_hum[2] = 255;
+// color_drops[0] = 0; color_drops[1] = 191; color_drops[2] = 255;
 ///////////Configurable Value///////////
+
+
 
 const String server = "http://opendata.artful.co.jp/get/?output=json";
 CRGB leds[NUM_LED];
+
 //environment_values
 String envVls;
 float temp, hum, press;
@@ -162,8 +175,8 @@ void led(int oclock, int dulationMin)
       }
       flag_mills = true;
     }
-    temp_bar(env_length[0]);
-    hum_bar(env_length[1]);
+    temp_bar(env_length[0], color_temp);
+    hum_bar(env_length[1], color_hum);
   }
 }
 
@@ -176,31 +189,70 @@ void setPix(int num,  CRGB rin[], int r, int g, int b)
 
 //===================env=====================//
 
-void temp_bar(int length_temp)
+void temp_bar(int length_temp, int color[3])
 {
   for(int i_temp = 0; i_temp <= length_temp; i_temp++)
   {
-    setPix(i_temp, leds, 100, 149, 237);
-    setPix(i_temp + 300, leds, 100, 149, 237);
+    setPix(i_temp, leds, color[0], color[1], color[2]);
+    setPix(i_temp + 300, leds, color[0], color[1], color[2]);
   }
   for(int i_temp = 299; i_temp >= length_temp; i_temp--)
   {
-    setPix(i_temp, leds, 100, 149, 237);
-    setPix(i_temp + 300, leds, 100, 149, 237);
+    setPix(i_temp, leds, color[0], color[1], color[2]);
+    setPix(i_temp + 300, leds, color[0], color[1], color[2]);
   }
 }
 
-void hum_bar(int length_hum)
+void hum_bar(int length_hum, int color[3])
 {
   for(int i_hum = 149; i_hum >= length_hum; i_hum--)
   {
-    setPix(i_hum, leds, 0, 191, 255);
-    setPix(i_hum + 300, leds, 0, 191, 255);
+    setPix(i_hum, leds, color[0], color[1], color[2]);
+    setPix(i_hum + 300, leds, color[0], color[1], color[2]);
   }
   for(int i_hum = 150; i_hum >= length_hum; i_hum++)
   {
-    setPix(i_hum, leds, 0, 191, 255);
-    setPix(i_hum + 300, leds, 0, 191, 255);
+    setPix(i_hum, leds, color[0], color[1], color[2]);
+    setPix(i_hum + 300, leds, color[0], color[1], color[2]);
+  }
+}
+
+bool flag_drops = true;
+bool flag_dropsSpeed = true;
+bool flag_drops_start = true;
+unsigned long time_drops;
+unsigned long time_dropsSpeed;
+int duration_drops;
+int i_temp_forDrops;
+int i_drops;
+void drops(int minDuration, int maxDuration, float duration_dropsSpeed, int color[3])
+{
+  if(flag_drops)
+  {
+    time_drops = millis();
+    duration_drops = random(minDuration, maxDuration);
+    flag_drops = false;
+  }
+  if(flag_dropsSpeed)
+  {
+    time_dropsSpeed = millis();
+    flag_dropsSpeed = false;
+  }
+  int eTime_dropsSpeed = millis() - time_dropsSpeed;
+  int eTime_drops = millis() - time_drops;
+  if(eTime_dropsSpeed >= duration_dropsSpeed)
+  {
+    if(flag_drops_start)
+    {
+      i_temp_forDrops = (int)env_length[0] + 1;
+      i_drops = 0;
+      flag_drops_start = false;
+    }
+    setPix(i_drops + i_temp_forDrops, leds, color[0], color[1], color[2]);
+    if(!i_drops == 0)
+      {
+        setPix(i_drops + i_temp_forDrops - 1, leds, 0, 0, 0);
+      }
   }
 }
 
