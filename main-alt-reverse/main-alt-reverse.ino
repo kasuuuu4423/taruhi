b1a7e36c9958ab3ffc560bf48ae533b89a8f02d6
kusakatakashi <tubomi10x29@gmail.com>
2020-01-20 15:56:16 +0900

    ver1/20-15:56

--- main-alt-reverse/main-alt-reverse.ino
+++ main-alt-reverse/main-alt-reverse.ino
@@ -12,7 +12,7 @@
 #define NUM_LED 600
 #define BRIGHTNESS 64
 #define PIN 0
-#define Oclock 20
+#define Oclock 18
 #define Minute 58
 const char* ssid = "kouheki";
 const char* pass = "kouheki0000";
